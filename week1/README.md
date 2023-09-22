bb5ba7b7232706e5c8c1f1c042677741afae55bc
Moira <mmccor25@jh.edu>
2023-09-22 09:36:29 -0400

    Exercise 2

---
+++ week1/README.md
@@ -0,0 +1,19 @@
+2.2
+	1. Size: The slope coefficient is 0.3776. This means that every year older the mother is, the rate of maternal de novo mutations tends to increase by 0.3776. The y-intercept, 2.5, is the number you would add to (.3776 * maternal age) to make a prediction of dnms based on maternal age (or hypothetically, how many dnms would result from a zero-year old mother lol)
+	
+	This matches the plot, because there seems to be a gradual upward trend in dnms with age.
+
+
+	2. This relationship is statistically significant because the p value is very small (less than .001)
+
+2.3 
+	1. Size: the slope coefficient is 1.3538. This means that every year older the father is, the rate of paternal de novo mutations tends to increase by 1.3538
+	This matches the plot, because there seems to be a gradual upward trend in dnms with age, but with a sharper increase than the maternal graph.
+
+
+
+2.4
+	y = mx + b
+	dnm = (1.3538)(age) + 10.3263
+	dnm = (1.3538)(50.1) + 10.3263
+	dnm = 78.15168
--- week1/wk1_assignment.py
+++ week1/wk1_assignment.py
@@ -34,4 +34,63 @@ age = pd.read_csv("aau1043_parental_age.csv", index_col = "Proband_id")
 #1.5
 merged = pd.concat([deNovoCountDF, age], axis = 1, join = 'inner')
 
+
 print(merged)
+# EXERCISE 2
+# 2.1
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+
+mat_age = merged.loc[:, 'Mother_age']
+mat_dnm = merged.loc[:, 'maternal_dnm']
+
+pat_age = merged.loc[:, 'Father_age']
+pat_dnm = merged.loc[:, 'paternal_dnm']
+
+fig1, mat = plt.subplots()
+mat.scatter(mat_age, mat_dnm)
+mat.set_title("Maternal de novo mutations")
+mat.set_xlabel("Maternal age (years)")
+mat.set_ylabel("Count of maternal de novo mutations")
+fig1.savefig( "ex2_a.png" )
+
+fig2, pat = plt.subplots()
+pat.scatter(pat_age, pat_dnm)
+pat.set_title("Paternal de novo mutations")
+pat.set_xlabel("Paternal age (years)")
+pat.set_ylabel("Count of paternal de novo mutations")
+fig2.savefig( "ex2_b.png" )
+
+# plt.show()
+# plt.close()
+
+
+# 2.2
+import statsmodels.formula.api as smf
+
+maternal = smf.ols(formula = "maternal_dnm ~ 1 + Mother_age", data = merged)
+m_results = maternal.fit()
+print(m_results.summary())
+
+# 2.3
+paternal = smf.ols(formula = "paternal_dnm ~ 1 + Father_age", data = merged)
+p_results = paternal.fit()
+print(p_results.summary())
+
+
+# 2.5
+
+fig3, both = plt.subplots()
+both.hist(mat_dnm, label = "maternal", alpha = 0.5)
+both.hist(pat_dnm, label = "paternal", alpha = 0.5)
+fig3.savefig( "ex2_c.png" )
+
+
+# plt.show()
+# plt.close()
+
+
+# 2.6
+import scipy.stats as sps
+t, p = sps.ttest_rel(mat_dnm, pat_dnm)
+print(p)
