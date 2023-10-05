25fea4c4a72c207eaead9f9ea6316fc79ead580d
Moira <mmccor25@jh.edu>
2023-10-05 19:55:03 -0400

    Complete

---
+++ week3/README.md
@@ -0,0 +1,12 @@
+
+$ python week3.py needleman-wunsch/CTCF_38_M27_AA.faa needleman-wunsch/BLOSUM62.txt -10 protein_output.txt
+PROTEIN:
+	Gaps in sequence 1: 9
+	Gaps in sequence 2: 9
+	Sequence alignment score: 3801.0
+
+$ python week3.py needleman-wunsch/CTCF_38_M27_DNA.fna needleman-wunsch/HOXD70.txt -300 DNA_output.txt
+DNA:
+	Gaps in sequence 1: 46
+	Gaps in sequence 2: 47
+	Sequence alignment score: 266300.0
--- week3/week3.py
+++ week3/week3.py
@@ -4,7 +4,7 @@ from fasta import readFASTA
 
 # EXERCISE 1
 
-# arguments: fasta_file, scoring_matrix.txt, gap_penalty, file path
+# arguments: fasta_file, scoring_matrix.txt, gap_penalty, file_path
 
 import sys
 import pandas as pd
@@ -22,6 +22,7 @@ seq2_id, sequence2 = input_sequences[1]
 F_matrix = np.zeros((len(sequence1)+1, len(sequence2)+1))
 traceback_matrix = np.zeros((len(sequence1)+1, len(sequence2)+1), dtype=str)
 
+file_path = sys.argv[4]
 
 # 1.3
 for i in range(len(sequence1)+1):
@@ -52,37 +53,55 @@ for i in range(1, len(sequence1)+1):
 		elif max(d, h, v) == v:
 			traceback_matrix[i,j] = "v"
 
-print(F_matrix)
-print(traceback_matrix)
-
 
 # 1.4
-sequence1_alignment = []
-sequence2_alignment = []
+sequence1_alignment = ""
+sequence2_alignment = ""
 
 i, j = len(sequence1), len(sequence2)
 while i >= 0 and j >= 0:
 	if traceback_matrix[i,j] == "d":
-		sequence1_alignment.insert(0, sequence1[i-1])
-		sequence2_alignment.insert(0, sequence2[j-1])
+		sequence1_alignment = sequence1[i-1] + sequence1_alignment
+		sequence2_alignment = sequence2[j-1] + sequence2_alignment
 		i -= 1
 		j -= 1
 	elif traceback_matrix[i,j] == "h":
-		sequence1_alignment.insert(0,"-")
-		sequence2_alignment.insert(0, sequence2[j-1])
+		sequence1_alignment = "-" + sequence1_alignment
+		sequence2_alignment = sequence2[j-1] + sequence2_alignment
 		j -= 1
 	elif traceback_matrix[i,j] == "v":
-		sequence1_alignment.insert(0, sequence1[i-1])
-		sequence2_alignment.insert(0,"-")
+		sequence1_alignment = sequence1[i-1] + sequence1_alignment
+		sequence2_alignment = "-" + sequence2_alignment
 		i -= 1
 	else:
 		i -= 1
 		j -= 1
 
-sequence1_string = ''.join(sequence1_alignment)
-sequence2_string = ''.join(sequence2_alignment)
+# count gaps
+gaps_1 = 0
+for i in range(len(sequence1_alignment)):
+	if sequence1_alignment[i] == "-":
+		gaps_1 += 1
 
-print(sequence1_string)
-print(sequence2_string)
+gaps_2 = 0
+for i in range(len(sequence2_alignment)):
+	if sequence2_alignment[i] == "-":
+		gaps_2 += 1
+
+# score alignment
+alignment_score = F_matrix[F_matrix.shape[0]-1, F_matrix.shape[1]-1]
+
+alignment1_output = "Sequence 1 alignment: " + sequence1_alignment + "\n"
+alignment2_output = "Sequence 2 alignment: " + sequence2_alignment + "\n"
+gaps1_output = "Gaps in sequence 1: " + str(gaps_1) + "\n"
+gaps2_output = "Gaps in sequence 2: " + str(gaps_2) + "\n"
+score_output = "Sequence alignment score: " + str(alignment_score)
+
+
+# 1.5
+output_file = open(file_path, "w")
+output_file.write(
+	alignment1_output + alignment2_output + gaps1_output + gaps2_output + score_output)
+
 
 	
