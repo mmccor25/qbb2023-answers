74098cfc1e1d949d5d3f3fcde253d17b19d097c5
Moira <mmccor25@jh.edu>
2023-11-24 19:51:01 -0500

    fix file names

--- week5/hw/alignment-variant_calling.sh
+++ week5/hw/alignment-variant_calling.sh
@@ -3,23 +3,30 @@
 cd /Users/cmdb/qbb2023-answers/week5/hw
 
 # 1.2
-# for sample in *.fastq
-# do 
-# 	echo $sample
-# 		bwa mem -t 4 -R "@RG\tID:${sample}\tSM:${sample}" \
-# 		sacCer3.fa \
-# 		${sample} > ${sample}.sam
-# done
+for sample in *.fastq
+do 
+	echo $sample
+		bwa mem -t 4 -R "@RG\tID:${sample}\tSM:${sample}" \
+		sacCer3.fa \
+		${sample} > ${sample%.*}.sam
+done
 
 # 1.3
-# for sam in *.sam
-# do
-# 	samtools sort -o `${sam} - ".sam"`.bam -O bam ${sam}
-# done
+for sam in *.sam
+do
+	samtools sort -o ${sam%.*}.bam -O bam ${sam}
+done
 
 for bam in *.bam
 do
 	samtools index ${bam}
-	freebayes -f sacCer3.fa -p 1 --genotype-qualities ${bam} > ${bam}.vcf
+	freebayes -f sacCer3.fa -p 1 --genotype-qualities ${bam} > ${bam%.*}.vcf
 done
 
+# 2.2
+for vcf in *.vcf
+do 
+	vcffilter -f "QUAL >= 30" vcf > ${vcf%.*}_quality.vcf
+done
+
+# 2.3
