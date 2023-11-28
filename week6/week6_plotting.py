2bf4cbbacba9347932bb82390b8b63931e586dc5
mmccor25 <144181767+mmccor25@users.noreply.github.com>
2023-11-28 18:32:49 -0500

    Manhattan Success!

--- week6/week6_plotting.py
+++ week6/week6_plotting.py
@@ -2,62 +2,99 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
-pc1 = []
-pc2 = []
+# pc1 = []
+# pc2 = []
 
-with open('pca.eigenvec', 'r') as file:
-	file = file.readlines()
-for line in file:
-	pc1.append(float(line[2]))
-	pc2.append(float(line[3]))
+# pca_file = pd.read_csv("pca.eigenvec", delim_whitespace=True)
+# pc1 = pca_file.iloc[:,2]
+# pc2 = pca_file.iloc[:,3]
 
-# Plot PCA
-pca, ax = plt.subplots()
-ax.scatter(pc1, pc2, alpha=0.5)
-ax.set_title("Genotypes PCA")
-ax.set_xlabel('PC 1')
-ax.set_ylabel('PC 2')
-# plt.show()
-pca.savefig("pca_1.png")
+# # Plot PCA
+# pca, ax = plt.subplots()
+# ax.scatter(pc1, pc2, alpha=0.5)
+# ax.set_title("Genotypes PCA")
+# ax.set_xlabel('PC 1')
+# ax.set_ylabel('PC 2')
+# # plt.show()
+# pca.savefig("pca_1.png")
 
-# EXERCISE 2
+# # EXERCISE 2
 
-# AFS
-af = []
-file = open("AF.frq", 'r')
-file = file.readlines()
-for line in file[1:]:
-	line = line.rstrip().split()
-	af.append(float(line[4]))
+# # AFS
+# af = []
+# file = open("AF.frq", 'r')
+# file = file.readlines()
+# for line in file[1:]:
+# 	line = line.rstrip().split()
+# 	af.append(float(line[4]))
 
-# histogram for AFS
-fig, ax = plt.subplots()
-ax.hist(af, bins=20, range=(0, .6))
-ax.set_title("Allele frequency spectrum")
-ax.set_xlabel("Allele frequency")
-# plt.show()
-fig.savefig("afs.png")
+# # histogram for AFS
+# fig, ax = plt.subplots()
+# ax.hist(af, bins=20, range=(0, .6))
+# ax.set_title("Allele frequency spectrum")
+# ax.set_xlabel("Allele frequency")
+# # plt.show()
+# fig.savefig("afs.png")
 
 
 
 # EXERCISE 3
+# set up GS451
 gs451 = pd.read_csv("gwas_GS451_IC50.assoc.linear", delim_whitespace=True)
-cb1908 = pd.read_csv("gwas_CB1908_IC50.assoc.linear", delim_whitespace=True)
 
-x_gs = gs451.loc[:,2]
-y_gs = gs451.loc[:,8]
+x_gs = gs451.index
+y_gs = gs451.loc[:,"P"]
 y_gs = -np.log(y_gs)
 
-x_cb = cb1908.loc[:,2]
-y_cb = gs451.loc[:,8]
+gs_insignificant_x = []
+gs_insignificant_y = []
+gs_significant_x = []
+gs_significant_y = []
+for x,y in zip(x_gs, y_gs):
+	if y > 5:
+		gs_significant_x.append(x)
+		gs_significant_y.append(y)
+	else:
+		gs_insignificant_x.append(x)
+		gs_insignificant_y.append(y)
+
+# set up CB1908
+cb1908 = pd.read_csv("gwas_CB1908_IC50.assoc.linear", delim_whitespace=True)
+
+x_cb = cb1908.index
+y_cb = cb1908.loc[:,"P"]
 y_cb = -np.log(y_cb)
 
+cb_insignificant_x = []
+cb_insignificant_y = []
+cb_significant_x = []
+cb_significant_y = []
+for x,y in zip(x_cb, y_cb):
+	if y > 5:
+		cb_significant_x.append(x)
+		cb_significant_y.append(y)
+	else:
+		cb_insignificant_x.append(x)
+		cb_insignificant_y.append(y)
+
+
+
+
+# make manhattan plot
 fig, ax = plt.subplots(2,1)
-ax[0,0].set_title("GS451")
-ax[0,0].scatter(x_gs, y_gs)
-ax[1,0].set_title("CB1908")
-ax[1,0].scatter(x_cb, y_cb)
+
+ax[0].set_title("GS451")
+ax[0].scatter(x_gs, y_gs)
+ax[0].scatter(gs_insignificant_x, gs_insignificant_y, color ="blue")
+ax[0].scatter(gs_significant_x, gs_significant_y, color="red")
+
+ax[1].set_title("CB1908")
+ax[1].scatter(x_cb, y_cb)
+ax[1].scatter(cb_insignificant_x, cb_insignificant_y, color ="blue")
+ax[1].scatter(cb_significant_x, cb_significant_y, color="red")
+
 fig.savefig("manhattan.png")
 
 
